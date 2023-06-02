097d41175d102f3890a70c950d24b73129645c63
CyberLynx1021 <abdulazizali234@gmail.com>
2023-06-02 01:46:00 -0500

    final project js 27%

--- anime.js
+++ anime.js
@@ -1,2 +1,12 @@
 // API 1: https://api.anidb.net:9001/httpapi?request=anime
-console.log('hero')
+let isModalOpen = false;
+
+
+function toggleModal() {
+    if (bento-menu) {
+        bentomenu = false;
+        return document.body.classList.remove("close__btn")
+    }
+    bentomenu = true;
+}
+element.onclick = 'close__btn'
