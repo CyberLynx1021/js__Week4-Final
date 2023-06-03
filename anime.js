eb7f4fe04b2c5807f2849d77bdf64a4d91052ca2
CyberLynx1021 <abdulazizali234@gmail.com>
2023-06-03 17:58:40 -0500

    final project js 29%

--- anime.js
+++ anime.js
@@ -1,12 +1,10 @@
 // API 1: https://api.anidb.net:9001/httpapi?request=anime
-let isModalOpen = false;
+// let isModalOpen = false;
 
-
-function toggleModal() {
-    if (bento-menu) {
-        bentomenu = false;
-        return document.body.classList.remove("close__btn")
-    }
-    bentomenu = true;
+function openMenu() {
+document.body.classList += ' menu--open'
 }
-element.onclick = 'close__btn'
+
+function closeMenu() {
+    document.body.classList.remove += 'menu--open'
+    }
