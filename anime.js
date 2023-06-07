a20d4de083f89be4e5be31826dc5496c3869aeec
CyberLynx1021 <abdulazizali234@gmail.com>
2023-06-06 22:05:39 -0500

    final project js 30 start on api start%

--- anime.js
+++ anime.js
@@ -1,5 +1,27 @@
-// API 1: https://api.anidb.net:9001/httpapi?request=anime
-// let isModalOpen = false;
+const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '50ea460c65msh9b251d8a5747921p1204a7jsn2eb5e50e7816',
+		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
+
+
+
+
+
+
 
 function openMenu() {
 document.body.classList += ' menu--open'
