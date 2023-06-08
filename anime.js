4477a46c77e98beecfa80227f188ff0ce5019677
CyberLynx1021 <abdulazizali234@gmail.com>
2023-06-08 03:44:00 -0500

    final project js 30 fixing

--- anime.js
+++ anime.js
@@ -1,22 +1,22 @@
-async function main(){
-const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '50ea460c65msh9b251d8a5747921p1204a7jsn2eb5e50e7816',
-		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
-	}
-};
+// async function main(){
+// const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '50ea460c65msh9b251d8a5747921p1204a7jsn2eb5e50e7816',
+// 		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+// 	}
+// };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
-main();
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+// }
+// main();
 
 
 // document.querySelector('')
